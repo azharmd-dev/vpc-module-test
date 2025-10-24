6d20bd818b0f438214cb9950644ba3809d27cfa2
azharmd-dev <azharmd0794@gmail.com>
2025-10-24 23:12:57 +0530

    Module to test VPC creation

--- main.tf
+++ main.tf
@@ -7,4 +7,7 @@ module "vpc" {
   vpc_cidr = var.vpc_cidr
   project_name = var.project_name
   environment = var.environment
+  public_subnet_cidr = var.public_subnet_cidr
+  private_subnet_cidr = var.private_subnet_cidr
+  database_subnet_cidr = var.database_subnet_cidr
 }
--- output.tf
+++ output.tf
@@ -1,3 +1,23 @@
 output "vpc_id" {
   value = module.vpc.vpc_id
 }
+
+output "public_subnet" {
+  value = module.vpc.public_subnet
+}
+
+output "private_subnet" {
+  value = module.vpc.private_subnet
+}
+
+output "database_subnet" {
+  value = module.vpc.database_subnet
+}
+
+output "ig_gw" {
+  value = module.vpc.ig_gw
+}
+
+output "eip" {
+  value = module.vpc.elastic_ip
+}
